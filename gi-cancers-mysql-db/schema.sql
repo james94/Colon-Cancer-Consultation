f071ff68de44ecd0f0eff3bcd150bb5f7d47b1c4
James Medel <jamesmedel94@gmail.com>
2022-04-28 06:34:19 -0700

    Update pcc_symptoms & pcc_riskfactors no table dep
    Updated  pcc_symptoms & pcc_riskfactors, so they dont have a dependency on patient table. Later on, I can incorporate more tables, such as that Patient table for storing more user data. For CMPE 252 demo, I will only use pcc_symptoms table insertion of user data

--- gi-cancers-mysql-db/schema.sql
+++ gi-cancers-mysql-db/schema.sql
@@ -3,9 +3,11 @@ drop database if exists gicancers;
 create database gicancers;
 use gicancers;
 
-SET @key_str = SHA2("The secret passphrase", 512)
-SET @init_vector = RANDOM_BYTES(16)
+-- SET @key_str = SHA2("The secret passphrase", 512);
+-- SET @init_vector = RANDOM_BYTES(16);
 -- CREATE Database Objects
+
+-- ONLY Testing "pcc_symptoms" for CMPE 252 Demo
 
 CREATE TABLE treatment (
     treatment_id varchar(9) not null,
@@ -36,20 +38,22 @@ CREATE TABLE patient (
     p_username varchar(30) not null,
     p_password blob not null,
     doctor_id varchar(9) not null,
+    rasa_id varchar(9) not null,
     unique(p_username),
     primary key (patient_id),
     foreign key (treatment_id) references treatment (treatment_id),
-    foreign key (doctor_id) references doctor (doctor_id)
+    foreign key (doctor_id) references doctor (doctor_id),
+    foreign key (rasa_id) references rasa_gicancer_va (rasa_id)
 );
 
 -- Rasa chatbot will record colon cancer symptoms and riskfactors
 -- into the following tables when surveying patient
 
 -- colon cancer symptoms
-CREATE TABLE p_cc_symptoms (
+CREATE TABLE pcc_symptoms (
     patient_id varchar(9) not null,
     p_name varchar(50) not null,
-    p_timestamp timestamp default current_timestamp,
+    p_timestamp varchar(9) not null,
     bowelh_changes varchar(50) not null,
     weakness boolean,
     fatigue boolean,
@@ -58,15 +62,14 @@ CREATE TABLE p_cc_symptoms (
     ab_discomfort varchar(50) not null,
     bowelne_feeling boolean,
     weight_loss_ue boolean,
-    primary key (patient_id, p_name, p_timestamp),
-    foreign key (patient_id) references patient (patient_id)
+    primary key (patient_id, p_name, p_timestamp)
 );
 
 -- colon cancer risk factors
-CREATE TABLE p_cc_riskfactors (
+CREATE TABLE pcc_riskfactors (
     patient_id varchar(9) not null,
     p_name varchar(50) not null,
-    p_timestamp timestamp default current_timestamp,
+    p_timestamp varchar(9) not null,
     age int not null,
     inherited_syndrome varchar(100) not null,
     sedentary_lifestyle boolean,
@@ -80,8 +83,7 @@ CREATE TABLE p_cc_riskfactors (
     colon_polyp_hist boolean,
     radiation_therap boolean,
     inflam_intest_cond boolean,
-    primary key (patient_id, p_name, p_timestamp),
-    foreign key (patient_id) references patient (patient_id)
+    primary key (patient_id, p_name, p_timestamp)
 );
 
 CREATE TABLE t_gicancer_area (
@@ -91,4 +93,35 @@ CREATE TABLE t_gicancer_area (
     foreign key (treatment_id) references treatment (treatment_id)
 );
 
+CREATE TABLE d_gicancer_area (
+    doctor_id varchar(9) not null,
+    cancer_name varchar(300) not null,
+    primary key (doctor_id, cancer_name),
+    foreign key (doctor_id) references doctor (doctor_id)
+);
 
+CREATE TABLE r_gicancer_area (
+    rasa_id varchar(9) not null,
+    cancer_name varchar(300) not null,
+    primary key (rasa_id, cancer_name),
+    foreign key (rasa_id) references rasa_gicancer_va (rasa_id)
+);
+
+
+CREATE TABLE researches (
+    rasa_id varchar(9) not null,
+    treatment_id varchar(9) not null,
+    primary key (rasa_id, treatment_id),
+    foreign key (rasa_id) references rasa_gicancer_va (rasa_id),
+    foreign key (treatment_id) references treatment (treatment_id)
+);
+
+CREATE TABLE examines (
+    doctor_id varchar(9) not null,
+    treatment_id varchar(9) not null,
+    primary key (doctor_id, treatment_id),
+    foreign key (doctor_id) references doctor (doctor_id),
+    foreign key (treatment_id) references treatment (treatment_id)
+)
+
+-- insert data from mayoclinic API
