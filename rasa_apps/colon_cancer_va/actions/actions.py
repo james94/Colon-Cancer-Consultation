660c4e34a3803792784e83387e3622115bf03491
James Medel <jamesmedel94@gmail.com>
2022-04-27 17:26:17 -0700

    Able to Run CCSymptoms Spider in Run X Times
    Was able to run my MayoClinicCCSymptoms spider multiple times in my run() method of ActionJoke class.
    
    Not able to run ActionSymptoms class yet.

--- rasa_apps/colon_cancer_va/actions/actions.py
+++ rasa_apps/colon_cancer_va/actions/actions.py
@@ -26,14 +26,30 @@
 #
 #         return []
 
-# import scrapy
-# from scrapy.crawler import CrawlerProcess
-# from collections import OrderedDict
-# from tqdm import tqdm
+import scrapy
+from collections import OrderedDict
+from tqdm import tqdm
+from scrapy.spiders import CrawlSpider
+import scrapydo
 
 import requests
 import json
 from rasa_sdk import Action
+
+cc_symptom_names = []
+
+class MayoClinicCCSymptomsSpider(scrapy.Spider):
+    name = "mc_cc_symptoms_spider"
+
+    def start_requests(self): # make an api request to mayoclinic
+        cc_symptoms_url = "https://www.mayoclinic.org/diseases-conditions/colon-cancer/symptoms-causes/syc-20353669"
+        yield scrapy.Request(url=cc_symptoms_url, callback=self.parse)
+
+    def parse(self, response):
+        cc_symptom_names = response.xpath('//h2[text()="Symptoms"]/following-sibling::ul[1]/li/text()').extract()
+        print("Printing CC Symptom Names")
+        print(cc_symptom_names)
+
 
 # 1. In 'data/stories.yml', replace 'utter_cheer_up' with the custom action 'action_joke'
 # tell your bot to use this new action
@@ -47,34 +63,52 @@ class ActionJoke(Action):
     def name(self):
         return "action_joke"
 
+    # def execute_crawling(self, dispatcher):
+    #     print("Creating Crawler Process")
+    #     process_symptoms = CrawlerProcess({
+    #         'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+    #     })
+    #     print("Passing MayoClinicCCSpider to crawl")
+    #     process_symptoms.crawl(MayoClinicCCSymptomsSpider)
+    #     process_symptoms.start()
+    #     print("Printing CC Symptoms in Action Joke")
+    #     print(cc_symptom_names)
+    #     dispatcher.utter_message(text="Testing CC Symptoms")
+
+
+    def run(self, dispatcher, tracker, domain):
+        # request = requests.get("http://api.icndb.com/jokes/random").json() # make an api call
+        # joke = request["value"]["joke"] # extract a joke from returned json response
+        # print(joke)
+        # dispatcher.utter_message(text=joke) # send the message back to the user
+        # return []
+
+        scrapydo.setup()
+        scrapydo.run_spider((MayoClinicCCSymptomsSpider))
+        dispatcher.utter_message(text="Testing CC Symptoms")
+
+        return []
+
+
+class ActionSymptoms(Action):
+    def name(self):
+        return "action_symptoms"
+
     def run(self, dispatcher, tracker, domain):
         request = requests.get("http://api.icndb.com/jokes/random").json() # make an api call
         joke = request["value"]["joke"] # extract a joke from returned json response
+        print(joke)
         dispatcher.utter_message(text=joke) # send the message back to the user
         return []
 
-# class FetchColonCancerSymptoms(Action):
-#     global cc_symptom_names
-#     cc_symptom_names = []
 
-#     class MayoClinicCCSymptomsSpider(scrapy.Spider):
-#         name = "mc_cc_symptoms_spider"
-
-#         def start_requests(self): # make an api request to mayoclinic
-#             cc_symptoms_url = "https://www.mayoclinic.org/diseases-conditions/colon-cancer/symptoms-causes/syc-20353669"
-#             yield scrapy.Request(url=cc_symptoms_url, callback=self.parse)
-
-#         def parse(self, response):
-#             cc_symptom_names = response.xpath('//h2[text()="Symptoms"]/following-sibling::ul[1]/li/text()').extract()
-        
-#     def name(self):
-#         return "fetch_cc_symptoms"
-
-#     def run(self, dispatcher, tracker, domain):
-#         process = CrawlerProcess({
-#             'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
-#         })
-#         process.crawl(self.MayoClinicCCSymptomsSpider)
-#         process.start()
-#         dispatcher.utter_message(text=cc_symptom_names)
-#         return []
+        # print("Creating Crawler Process")
+        # process = CrawlerProcess({
+        #     'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+        # })
+        # print("Passing MayoClinicCCSpider to crawl")
+        # process.crawl(MayoClinicCCSymptomsSpider)
+        # process.start()
+        # print(cc_symptom_names)
+        # dispatcher.utter_message(text="Testing CC Symptoms")
+        # return []
